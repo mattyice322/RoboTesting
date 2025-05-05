6e49c266764376707e464c50a3170dc46d7d845d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-05 17:14:48 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -43,13 +43,13 @@ void main() async {
     ));
     await GoogleFonts.pendingFonts();
 
-    await tester.pumpAndSettle(const Duration(milliseconds: 50000));
+    await tester.pumpAndSettle(const Duration(milliseconds: 5000));
     await tester.enterText(
         find.byKey(const ValueKey('loginEmail_gkw6')), 'jess@gmail.com');
     await tester.enterText(
         find.byKey(const ValueKey('loginPassword_ps56')), 'csc305');
     await tester.tap(find.byKey(const ValueKey('LoginButton_i83y')));
-    await tester.pumpAndSettle(const Duration(milliseconds: 50000));
+    await tester.pumpAndSettle(const Duration(milliseconds: 5000));
     expect(find.byKey(const ValueKey('Column_o84v')), findsWidgets);
   });
 
